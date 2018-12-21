5b7a2fa0f36728c53563b5a9766ce51eb06988c2
arcmachines <wolfram.donat@arcmachines.org>
2018-12-21 09:15:45 -0800

    Updating GPS code and autostart file

--- display_images.py
+++ display_images.py
@@ -4,36 +4,89 @@
 import time
 import psutil
 import os
+from gps import *
+import threading
+
+class GpsPoller(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        global gpsd
+        gpsd = gps(mode=WATCH_ENABLE)
+        self.current_value = None
+        self.running = True
+    def run(self):
+        global gpsd
+        while gpsp.running:
+            gpsd.next()
 
 def displayImage(img_dir, dur):
-  # Display image
-  os.system('feh --hide-pointer -x -q black -g 1366x768 ' + img_dir + ' &')
-  time.sleep(dur)
+    # Display image
+    os.system('feh --hide-pointer -x -q black -g 1366x768 ' + img_dir + ' &')
+    time.sleep(dur)
 
-  # Clear image
-  for process in psutil.process_iter():
-    if 'feh' in process.cmdline():
-      process.terminate()
-  return
-
-def main():
-
-  images = ['JP', 'AD', 'logo', 'AA', 'web', 'lakers']
-  JP = os.path.expanduser('~/ADWAY/JP')
-  AD = os.path.expanduser('~/ADWAY/AD')
-  logo = os.path.expanduser('~/ADWAY/logo')
-  AA = os.path.expanduser('~/ADWAY/AA')
-  web = os.path.expanduser('~/ADWAY/web')
-  lakers = os.path.expanduser('~/ADWAY/lakers')
-
-  while True:
-      displayImage(JP, 8)
-      displayImage(AD, 10)
-      displayImage(logo, 5)
-      displayImage(AA, 10)
-      displayImage(web, 15)
-      displayImage(lakers, 8)
+    # Clear image
+    for process in psutil.process_iter():
+        if 'feh' in process.cmdline():
+            process.terminate()
+    return
 
 
 if __name__ == '__main__':
-  main()
+    gpsp = GpsPoller()
+    JP = os.path.expanduser('~/ADWAY/JP')
+    AD = os.path.expanduser('~/ADWAY/AD')
+    logo = os.path.expanduser('~/ADWAY/logo')
+    AA = os.path.expanduser('~/ADWAY/AA')
+    web = os.path.expanduser('~/ADWAY/web')
+    try:
+        gpsp.start()
+        while True:
+
+            if gpsd.fix.latitude > 0:
+                lat = 'N'
+            else:
+                lat = 'S'
+            latString = str(abs(round(gpsd.fix.latitude, 2)))
+            lonString = str(abs(round(gpsd.fix.longitude, 2)))
+            dateString = str(gpsd.utc)
+            with open('/home/pi/ADWAY/gps.txt', 'a') as f:
+                f.write('JP - ' + dateString + '\n')
+                f.write(latString + ', ' + lonString + '\n')
+            displayImage(JP, 8)
+
+            latString = str(abs(round(gpsd.fix.latitude, 2)))
+            lonString = str(abs(round(gpsd.fix.longitude, 2)))
+            dateString = str(gpsd.utc)
+            with open('/home/pi/ADWAY/gps.txt', 'a') as f:
+                f.write('AD - ' + dateString + '\n')
+                f.write(latString + ', ' + lonString + '\n')
+            displayImage(AD, 10)
+
+            latString = str(abs(round(gpsd.fix.latitude, 2)))
+            lonString = str(abs(round(gpsd.fix.longitude, 2)))
+            dateString = str(gpsd.utc)
+            with open('/home/pi/ADWAY/gps.txt', 'a') as f:
+                f.write('logo - ' + dateString + '\n')
+                f.write(latString + ', ' + lonString + '\n')
+            displayImage(logo, 5)
+
+            latString = str(abs(round(gpsd.fix.latitude, 2)))
+            lonString = str(abs(round(gpsd.fix.longitude, 2)))
+            dateString = str(gpsd.utc)
+            with open('/home/pi/ADWAY/gps.txt', 'a') as f:
+                f.write('AA - ' + dateString + '\n')
+                f.write(latString + ', ' + lonString + '\n')
+            displayImage(AA, 10)
+
+            latString = str(abs(round(gpsd.fix.latitude, 2)))
+            lonString = str(abs(round(gpsd.fix.longitude, 2)))
+            dateString = str(gpsd.utc)
+            with open('/home/pi/ADWAY/gps.txt', 'a') as f:
+                f.write('web - ' + dateString + '\n')
+                f.write(latString + ', ' + lonString + '\n')
+            displayImage(web, 15)
+            
+    except(KeyboardInterrupt, SystemExit):
+        gpsp.running = False
+        gpsp.join()
+            
